ba287d3b23ce6d32ef7bd77004367b3e4dcf5c13
Vitória <129441807+vitoriavianaa@users.noreply.github.com>
2023-05-02 11:49:55 -0300

    Add files via upload

---
+++ empacotamento_e_desempacotamento.py
@@ -0,0 +1,7 @@
+lista = ['Vitória', 'Vic', 'Faeneri', 'Fae']
+nome1, nome2, nome3, nome4 = lista
+print(nome3)
+
+lista = ['Vitória', 'Vic', 'Faeneri', 'Fae']
+nome1, *resto = lista
+print(nome1, resto)
